5c14f8d38b35bf2f4c817ad0c7b829a62d3fd975
Zemouri <otman.zemouri@sogeti.com>
2022-06-22 11:34:47 +0200

    Refactoring lay-out text

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('React Test');
+  const linkElement = screen.getByText('React Test App');
   expect(linkElement).toBeInTheDocument();
 });
